aee3a0d607556dcc3204b3b9e5d79ef5b01927d7
Ilia <idoffix@gmail.com>
2024-01-13 19:35:46 +0300

    kafka+db=works,but..

--- master.go
+++ master.go
@@ -5,7 +5,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/IBM/sarama"
 	_ "github.com/lib/pq"
 )
 
@@ -18,7 +22,6 @@ const (
 )
 
 // инициализируем соединение с БД
-//var db *sql.DB
 
 func Init() *sql.DB {
 
@@ -44,7 +47,7 @@ func Init() *sql.DB {
 
 func checkErr(err error) {
 	if err != nil {
-		fmt.Printf("Ошибка ???, %s", err)
+		fmt.Printf("Ошибка, %s", err)
 		panic(err)
 	}
 }
@@ -63,7 +66,7 @@ func main() {
 
 	mux.HandleFunc("/plotnikov/db", GetInfo)
 
-	mux.HandleFunc("/plotnikov/db/post", PostInfo)
+	mux.HandleFunc("/plotnikov/db_post", PostInfo)
 
 	//server
 
@@ -71,6 +74,7 @@ func main() {
 		Addr:    ":1311",
 		Handler: mux,
 	}
+
 	s.ListenAndServe()
 
 }
@@ -111,6 +115,7 @@ type JsonResponse struct {
 	Data []info_js `json:"data"`
 }
 
+// Fetch db
 func GetInfo(res http.ResponseWriter, req *http.Request) {
 
 	db := Init()
@@ -157,30 +162,143 @@ func GetInfo(res http.ResponseWriter, req *http.Request) {
 func PostInfo(w http.ResponseWriter, r *http.Request) {
 
 	comment := r.FormValue("comment")
+	mesg, _ := json.Marshal(comment)
 
 	var response = JsonResponse{}
 
 	if comment == "" {
 		response = JsonResponse{Type: "error"}
 	} else {
-		db := Init()
-		// dynamic
-		insertDynStmt := `insert into "test"("comment") values($1)`
-
-		_, err := db.Exec(insertDynStmt, comment)
-
-		checkErr(err)
-
-		printMessage("Inserting comment into DB")
-
-		//var lastInsertID int
-		//err := db.QueryRow("INSERT INTO test (comment) VALUES($1);", comment).Scan(&lastInsertID)
-
-		// check errors
-		//checkErr(err)
-
-		response = JsonResponse{Type: "success"}
+		PushCommentToQueue("comments", mesg)
+		go consume()
 	}
 
 	json.NewEncoder(w).Encode(response)
 }
+
+// .................Producer........................
+//
+// Инициализируем соединение с кафкой продюсером
+func ConnectProducer(brokersUrl []string) (sarama.SyncProducer, error) {
+	fmt.Println("producer trying to connect")
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+	//NewSyncProducer creates a new SyncProducer using the given broker addresses and configuration.
+	conn, err := sarama.NewSyncProducer(brokersUrl, config)
+	if err != nil {
+		printMessage("Connection failed")
+		return nil, err
+	}
+	return conn, nil
+}
+
+// Отправляем сообщение в топик кафки
+func PushCommentToQueue(topic string, message []byte) error {
+	fmt.Println("producer starts init")
+	brokersUrl := []string{"kafka:9092"}
+	producer, err := ConnectProducer(brokersUrl)
+	if err != nil {
+		return err
+	}
+	defer producer.Close()
+
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.StringEncoder(message),
+	}
+	partition, offset, err := producer.SendMessage(msg)
+
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Message is stored in topic(%s)/partition(%d)/offset(%d)\n", topic, partition, offset)
+	return nil
+}
+
+//...........................................
+
+// ..............Consumer..................
+// Коннектимся к кафке консъюмером
+func connectConsumer(brokersUrl []string) (sarama.Consumer, error) {
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
+	// NewConsumer creates a new consumer using the given broker addresses and configuration
+	conn, err := sarama.NewConsumer(brokersUrl, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func consume() {
+
+	printMessage("Consumer started ")
+
+	topic := "comments"
+	worker, err := connectConsumer([]string{"kafka:9092"})
+	if err != nil {
+		panic(err)
+	}
+
+	// Calling ConsumePartition. It will open one connection per broker
+	// and share it for all partitions that live on it.
+	consumer, err := worker.ConsumePartition(topic, 0, sarama.OffsetOldest)
+	if err != nil {
+		panic(err)
+	}
+
+	sigchan := make(chan os.Signal, 1)
+
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
+	// Count how many message processed
+	msgCount := 0
+
+	// Get signal for finish
+	doneCh := make(chan struct{})
+
+	go func() {
+		for {
+			select {
+			case err := <-consumer.Errors():
+
+				fmt.Println(err)
+
+			case msg := <-consumer.Messages():
+
+				msgCount++
+
+				fmt.Printf("Received message Count %d: | Topic(%s) | Message(%s) \n", msgCount, string(msg.Topic), string(msg.Value))
+
+				cmt := string(msg.Value)
+
+				db := Init()
+				// dynamic
+				insertDynStmt := `insert into "test"("comment") values($1)`
+
+				_, err_db := db.Exec(insertDynStmt, cmt)
+
+				checkErr(err_db)
+
+				printMessage("Inserting comment into DB")
+
+			case <-sigchan:
+				fmt.Println("Interrupt is detected")
+				doneCh <- struct{}{}
+			}
+		}
+	}()
+
+	<-doneCh
+	fmt.Println("Processed", msgCount, "messages")
+
+	if err := worker.Close(); err != nil {
+		panic(err)
+	}
+
+}
+
+//...........................................
